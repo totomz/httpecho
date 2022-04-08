6f4c6f5764ad678b0239cfaea45bcca9922981b8
tommaso.doninelli <tommaso.doninelli@my-ideas.it>
2022-04-08 11:34:01 +0200

    Added endpoint to test upload

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ HTTPEcho listen to the port `8080` for incoming HTTP GET requests on any path, a
 Check the `httpecho.yaml` for the resource definition, and then `kubectl apply -f httpecho.yaml`  
 
 The container image is [totomz84/httpecho](https://hub.docker.com/r/totomz84/httpecho)
+
+# Test upload
+```bash
+curl -X POST  -F file=@Syleak_Brand.zip  http://localhost:8080/upload
+```
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -13,13 +15,16 @@ var (
 	GitCommit string
 )
 
+// TODO Output in JSON
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	http.HandleFunc("/", Handler)
+	http.HandleFunc("/upload", HandlerUpoad)
+	http.HandleFunc("/", HandlerEcho)
 
 	stdout.Printf("Listening on port %s", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
@@ -27,16 +32,76 @@ func main() {
 	}
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+func HandlerEcho(w http.ResponseWriter, r *http.Request) {
 	requestUrl := r.RequestURI
 	log.Printf("got reguest for %s", requestUrl)
 
-	fmt.Fprintf(w, "HELLO! GitCommit: %s  BuildTime: %s\n", GitCommit, BuildTime)
-	fmt.Fprintf(w, "Request url :%s\n", requestUrl)
-	fmt.Fprintf(w, "{ \n")
-	fmt.Fprintf(w, "    Host => %s\n", r.Host)
-	for k, v := range r.Header {
-		fmt.Fprintf(w, "    %s => %s\n", k, v)
+	res := map[string][]string{
+		"helo": {fmt.Sprintf("HELLO! GitCommit: %s  BuildTime: %s\n", GitCommit, BuildTime)},
+		"url":  {requestUrl},
+		"Host": {r.Host},
 	}
-	fmt.Fprintf(w, "} \n")
+
+	for k, v := range r.Header {
+		res[k] = v
+	}
+
+	js, _ := json.Marshal(res)
+	w.Header().Add("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func HandlerUpoad(w http.ResponseWriter, r *http.Request) {
+	requestUrl := r.RequestURI
+	log.Printf("got reguest for %s", requestUrl)
+	file, header, err := r.FormFile("file")
+
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+	defer file.Close()
+
+	out, err := os.Create(header.Filename)
+	if err != nil {
+		fmt.Fprintf(w, "Unable to create the file for writing. Check your write access privilege")
+		return
+	}
+
+	defer out.Close()
+
+	_, err = io.Copy(out, file)
+	if err != nil {
+		fmt.Fprintln(w, err)
+	}
+
+	log.Printf("file saved %s", header.Filename)
+
+	savedFile, errOpen := os.Open(header.Filename)
+	if errOpen != nil {
+		w.WriteHeader(500)
+		fmt.Fprintln(w, err)
+	}
+	savedFileStat, errStat := savedFile.Stat()
+	if errStat != nil {
+		w.WriteHeader(500)
+		fmt.Fprintln(w, err)
+	}
+
+	res := map[string][]string{
+		"name": {savedFileStat.Name()},
+		"size": {fmt.Sprintf("%v", savedFileStat.Size())},
+	}
+
+	js, _ := json.Marshal(res)
+	w.Header().Add("Content-Type", "application/json")
+	w.Write(js)
+
+	defer func() {
+		errRemove := os.Remove(header.Filename)
+		if errRemove != nil {
+			log.Printf("unable to remove temp file %v", errRemove)
+		}
+	}()
+
 }
